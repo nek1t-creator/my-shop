55e0d5fe3676874c46529587017dfa5fe688b0f7
Nikita <nikta567killglol@gmail.com>
2025-07-03 14:04:08 +0500

    Updates

---
+++ _next/static/chunks/app/layout-221ee8075739d6f4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3339:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,7656,23)),Promise.resolve().then(a.t.bind(a,9324,23))},7656:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_c014a7"}},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[721,441,684,358],()=>s(3339)),_N_E=e.O()}]);
